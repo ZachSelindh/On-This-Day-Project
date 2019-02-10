221b987416f31c7432739387a5e469b8e13d6add
Raymond Fwu <rayfwu@gmail.com>
2019-02-10 12:47:17 -0600

    made the dates in the dropdown more viewer friendly while preserving the json filenames

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,18 +2,20 @@
 
 $(document).ready(function() {
 
-var listItems = [   'february-14', 
-                  'february-20', 
-                  'february-29', 
-                  'march-3',
-                  'april-1',
-                  'june-21',
-                  'july15',
-                  'nov-20',
-                  'december-7'];
+// array of objects that contain a viewer friendly date and a filename date
+
+var listItems = [ {display : 'February 14th', filename : 'february-14'},
+                  {display : 'February 20th', filename :'february-20'},
+                  {display : 'February 29th', filename :'february-29'}, 
+                  {display : 'March 3rd', filename : 'march-3'},
+                  {display : 'April 1st', filename : 'april-1'},
+                  {display : 'June 21st', filename : 'june-21'},
+                  {display : 'July 15th', filename : 'july15'},
+                  {display : 'November 20th', filename : 'nov-20'},
+                  {display : 'December 7th', filename :'december-7'}];
 
 for (let x in listItems) {
-    var anItem = "<div id='item-" + x + "' class='dropdown-item' data-json='assets/json/" + listItems[x] + ".json'>" + listItems[x] + "</div>";
+    var anItem = "<div id='item-" + x + "' class='dropdown-item' data-json='assets/json/" + listItems[x].filename + ".json'>" + listItems[x].display + "</div>";
     $("#list-date").append(anItem);
     $("#item-" + x).click( function() {
         console.log($("#item-" + x).data("json"));
