66adff4bbe79f6c7bc8cc62a44fe1f89f63b9c62
Raymond Fwu <rayfwu@gmail.com>
2019-02-12 20:02:52 -0600

    added random number feature to get random event from date as well as code to deal with cases where date wasn't chosen

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,7 @@
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 $(document).ready(function() {
 
@@ -16,39 +19,46 @@ $(document).on("click", "#search-button", function(){
 
   // takes the input from the datepicker and makes an api call to get the events
   var date = $("#day-input").val();
-  var wikiUrl = "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=1&page=" 
-                + date 
-                + "&callback=?";
 
-	$.ajax({
-	    type: "GET",
-	    url: wikiUrl,
-	    contentType: "application/json; charset=utf-8",
-	    async: false,
-	    dataType: "json",
-	    success: function (data, textStatus, jqXHR) {
-	    
-		var markup = data.parse.text["*"];
-		var i = $('<div></div>').html(markup);
-		
-		// remove links as they will not work
-		i.find('a').each(function() { $(this).replaceWith($(this).html()); });
-		
-		// remove any references
-		i.find('sup').remove();
-		
-		// remove cite error
-		i.find('.mw-ext-cite-error').remove();
+  if (date == "") {
+    $("#wiki-display").text("");
+  } else {
+    var wikiUrl = "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=1&page=" 
+    + date 
+    + "&callback=?";
 
-    // initial array to hold the events
-		var events = [];
+    $.ajax({
+      type: "GET",
+      url: wikiUrl,
+      contentType: "application/json; charset=utf-8",
+      async: false,
+      dataType: "json",
+      success: function (data, textStatus, jqXHR) {
+
+      var markup = data.parse.text["*"];
+      var i = $('<div></div>').html(markup);
+
+      // remove links as they will not work
+      i.find('a').each(function() { $(this).replaceWith($(this).html()); });
+
+      // remove any references
+      i.find('sup').remove();
+
+      // remove cite error
+      i.find('.mw-ext-cite-error').remove();
+
+      // initial array to hold the events
+      var events = [];
       $(i).find('li').each( function() { events.push($(this).text()); });
-      $("#wiki-display").text(events[0]);
-      console.log(events[0]);
-    },
-    error: function (errorMessage) {
-    }
-	});   
+        $("#wiki-display").text(events[getRandomInt(events.length)]);
+        console.log(events[getRandomInt(events.length)]);
+      },
+      error: function (errorMessage) {
+      }
+    }); 
+  }
+
+  
 
     // var searchTerm = $("#day-input").val();
 
